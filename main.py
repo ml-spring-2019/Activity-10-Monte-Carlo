f9e5f93fed9f25317abaae816ce699a7acb44def
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2019-04-06 22:01:23 -0700

    my laptop may be broken but the cluster isn't

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ def main():
     state_num, action_num = 1, 2
     create_graph()
     ending_states = transition(state_num, action_num)
-    print(generate_episode("state_1"))
+    result = generate_episode("state_1")
+    print_result(result)
 
+def print_result(result):
+    for i in range(0, len(result)):
+        dict = result[i]
+        if i == len(result) - 1:
+            print(dict["state"] + "(+" + str(dict["reward"]) + ")" )
+        else:
+            print(dict["state"] + "(" + str(dict["action"]) + ", " + str(dict["reward"]) + ")" )
 
 def create_graph():
     global graph
@@ -31,16 +39,46 @@ def reward(action_state, ending_state):
 def generate_episode(start_state):
     global graph
     steps = ""
+    result = []
     if start_state == "state_5":
-        return "state_5"
+        return [{"state": start_state, 
+                 "reward": 20}]
 
     # randomize an action and get tuple
-    act_tup = transition(int(start_state[-1]), random.randint(1,2))
+    action = random.randint(1,2)
+    act_tup = transition(int(start_state[-1]), action)
 
-    for state in act_tup:
+    #for state in act_tup:
 #        pdb.set_trace()
-        steps = generate_episode(state)
-    return start_state + " " + steps
+    branch = random.randint(1, len(act_tup)) - 1
+    branch_result = []
+    for act in act_tup:
+        dest_state = act
+        if action == 1:
+            my_result = [{"state": start_state, 
+                          "action": action, 
+                          "reward": -1}]
+        elif action == 2:
+            my_result = [{"state": start_state,
+                          "action": action,
+                          "reward": -2}]
+        else:
+            print("UNKNOWN ACTION " + str(action))
+            exit(1)
+        my_result.extend(generate_episode(dest_state))
+        branch_result.append(my_result)
+    if len(branch_result) == 1:
+        return branch_result[0]
+    else:
+        # find branch with highest reward
+        rewards = []
+        for b in branch_result:
+            my_reward = 0
+            for d in b:
+                my_reward += d["reward"]
+            rewards.append(my_reward)
+        index_of_highest_reward = rewards.index(max(rewards))
+        return branch_result[index_of_highest_reward]
 
 if __name__ == "__main__":
     main()
